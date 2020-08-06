e562c81b26323a03b24bdab84a2471dab3f767b0
AntonPshen <p.anton.vladimirovich@gmail.com>
2020-08-06 22:21:55 +0300

    Creating Users and Calling Their Values

--- src/User.java
+++ src/User.java
@@ -30,6 +30,15 @@ public class User {
     }
 
     public static void main(String[] args) {
+        User bill = new User("Bill", (short) 23, 165);
+        User kate = new User("Kate", (short) 25, 158);
+        User jack = new User("Jack", 176, (short) 33);
+        User masha = new User("Masha", 163, (short) 28);
+        User michael = new User((short) 63, "Michael", 180);
+        User sara = new User((short) 65, "Sara", 167);
 
+        System.out.println(bill.name + " " + bill.age);
+        System.out.println(jack.name + " " + jack.age);
+        System.out.println(michael.name + " " + michael.age);
     }
 }
